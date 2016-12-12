172119a99d168145448bcb7b41d0ae72d909affe
dieptran <dieptx.ptit@gmail.com>
2016-12-13 00:37:01 +0700

    Fix meaning filter word

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to categories_path
+      redirect_to user
     else
       flash[:danger] = t "flash.error"
       render :new
